285f462be59c19c98f79052c464c8f2b09c3ef50
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2022-02-25 03:09:38 +0100

    refactor

--- appointments/show-months-appointments.js
+++ appointments/show-months-appointments.js
@@ -57,8 +57,8 @@ function load_calendar(calender_count = 0) {
     if(empty_start_dates_count > 0) {
         has_prev_month = true;
 
-        for(let i = date_ranges.length; i > empty_start_dates_count; i--) {
-            const prev_date = moment().add(calender_count-1, 'month').endOf('month').add(empty_start_dates_count - i, 'days').format('D');
+        for(let i = 0; i < empty_start_dates_count; i++) {
+            const prev_date = moment().add(calender_count-1, 'month').endOf('month').add(0 - i, 'days').format('D');
             append_prev_month_el += '<div class="div-block-14 blank-cell"><div class="text-block-6">' + prev_date + '</div></div>';
         }
     }
@@ -142,6 +142,8 @@ function getAllBookedAppointments() {
     inner_page_loader.setAttribute('style', 'display:flex');
 
     appointments = [];
+
+
 
 }
 
