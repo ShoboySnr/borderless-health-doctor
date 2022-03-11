b2cc589ceecf82c080fe0076de245e9bbc14f323
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2022-03-11 19:24:10 +0100

    append buttons

--- video-call/app.js
+++ video-call/app.js
@@ -150,6 +150,7 @@ async function connectVideo(token, roomName, inner_loader, event) {
     cancelButton.addEventListener('click', (event) => {
         let element = event.target;
         room.on('disconnected', room => {
+            console.log(room);
             // Detach the local media elements
             room.localParticipant.tracks.forEach(publication => {
                 const attachedElements = publication.track.detach();
